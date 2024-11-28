afa1ba3c6b64b29d042cac1fab9c7582c14f0677
AkmalRendiansyah <akmalrendiansyah@gmail.com>
2024-11-28 11:54:01 +0700

    add result

--- mobile-development/app/build.gradle.kts
+++ mobile-development/app/build.gradle.kts
@@ -61,7 +61,6 @@ dependencies {
     implementation (libs.androidx.camera.camera2)
     implementation (libs.androidx.camera.lifecycle)
     implementation (libs.androidx.camera.view)
-
-    implementation("org.tensorflow:tensorflow-lite-task-vision:0.1.0")
+    implementation(libs.tensorflow.tensorflow.lite.task.vision)
 
 }
